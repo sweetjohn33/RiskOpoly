38c7a1bd89c2958fc65a1fa42d3d98c8273d21a0
Brett Henderson <brettrhenderson25@gmail.com>
2018-03-14 01:06:04 -0400

    first commitgit status!

---
+++ objects/cards.py
@@ -0,0 +1,28 @@
+# A module to define a game card and all of its subtypes
+
+class Card:
+    """A generic card used in gameplay"""
+
+    def __init__(self, deck):
+        self.add_to_deck(deck)
+        self.deck = deck
+
+    def add_to_deck(self, deck):
+        # add_card to deck
+        pass
+
+    def draw_card(self, player):
+        # a player draws a card
+        pass
+
+    def flip(self):
+        # flip a card over to reveal face
+        pass
+
+    def play(self):
+        # play the card
+        pass
+
+    def display(self):
+        # draw the card on the game board with whichever face should be up
+        pass
